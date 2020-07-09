dfe4a8ffff326170ba9451cd500645ad1e3cac00
hakyoungJeong <hakyoung1215@gmail.com>
2020-07-09 23:25:46 +0900

    7주차

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -6,10 +7,20 @@ class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to="blog/", blank=True, null=False)
+    image = models.ImageField(upload_to="blog/", blank=True, null=True)
     #media/blog/파일이름 으로 저장되게해줌
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likers')
     def __str__(self):
         return self.title
 
     def summary(self):
         return self.body[:100]
+
+class Comment(models.Model):
+    blog =  models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True, related_name='comments')
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}님이 {self.blog}에 단 댓글"
